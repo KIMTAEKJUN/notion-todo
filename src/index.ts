c84ede1eb0c204c042dbee3cbb1f586a4afdeb07
TaekJun Kim <gdsygrt557422@gmail.com>
2024-10-25 09:17:49 +0900

    feat: Add error handling and comments to code

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,48 @@ import { getDateStr, getLastWorkday, isWeekend } from "./utils/date";
 const notionService = new NotionService();
 const slackService = new SlackService();
 
+process.on("uncaughtException", (error) => {
+  console.error("uncaughtException 발생:", error);
+  slackService
+    .sendNotification(
+      "🚨 Notion TODO 서비스에 예기치 못한 에러가 발생했습니다.",
+      "🧑🏻‍💻 서비스를 확인해주세요.",
+      [],
+      []
+    )
+    .catch(console.error);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("unhandledRejection 발생:", reason, promise);
+  slackService
+    .sendNotification(
+      "🚨 Notion TODO 서비스에 예기치 못한 에러가 발생했습니다.",
+      "🧑🏻‍💻 서비스를 확인해주세요.",
+      [],
+      []
+    )
+    .catch(console.error);
+});
+
 async function runDailyTodo() {
   const lastWorkday = getLastWorkday();
   const lastWorkdayStr = getDateStr(lastWorkday);
 
+  // 주말인 경우 실행하지 않음
   if (isWeekend()) {
     return;
   }
 
   try {
-    // 1단계
+    // 1. 이전 날짜의 미완료 TODO 항목들을 가져옴
     const { pendingTodos, inProgressTodos } =
       await notionService.getYesterdayUncompletedTodos();
 
-    // 2단계
+    // 2. 금일의 TODO 페이지를 생성
     const response = await notionService.createDailyTodo();
 
-    // 3단계
+    // 3. Slack으로 알림 전송
     await slackService.sendNotification(
       `📅 *금일 [${getDateStr()}]의 TODO가 생성되었습니다.*`,
       `🛵 *전날 [${lastWorkdayStr}]의 미완료 항목이 이전되었습니다.*`,
@@ -40,24 +65,11 @@ async function runDailyTodo() {
   }
 }
 
-// 매일 아침 8시에 실행, 주말에는 실행하지 않음
-cron.schedule("0 8 * * 1-5", async () => {
+// 평일 아침 8시 30분에 실행
+cron.schedule("30 8 * * 1-5", async () => {
   try {
     await runDailyTodo();
   } catch (error) {
     console.error("CRON 작업 중 오류가 발생했습니다.", error);
   }
 });
-
-if (process.argv.includes("--test")) {
-  console.log("테스트 실행 시작...");
-  runDailyTodo()
-    .then(() => {
-      console.log("테스트 실행 완료");
-      process.exit(0); // 성공적으로 완료되면 프로세스 종료
-    })
-    .catch((error) => {
-      console.error("테스트 실행 실패:", error);
-      process.exit(1); // 에러가 있으면 에러 코드와 함께 종료
-    });
-}
